184573067dd3b473e70aff961cd56023ae6a98ca
Luciano <delahayeluciano9@gmail.com>
2024-05-14 22:58:15 -0300

    Modified class 12 form according to class 13 exercise

--- clase 12 - ejercicios/ejercicio-1/scripts.js
+++ clase 12 - ejercicios/ejercicio-1/scripts.js
@@ -1,5 +1,7 @@
-const MAX_NAME_LENGTH = 30
+const MAX_NAME_LENGTH = 30;
 
+const allNames = document.querySelectorAll(".name");
+const allPasswords = document.querySelectorAll(".password");
 
 const firstName = document.getElementById("first-name");
 const lastName = document.getElementById("last-name");
@@ -9,57 +11,122 @@ const confirmpw = document.getElementById("confirm-pw");
 const birthday = document.getElementById("birthday");
 const checkbox = document.getElementById("newsletter-checkbox");
 
+const warnings = document.querySelectorAll(".warning")
+
 const button = document.querySelector("button");
 
-const validate = () => {
-    
-    let isValid = true
+//flags
+let isFirstNameValid = true;
+let isLastNameValid = true;
+let isPasswordValid = true;
 
-    isValid = firstName.value.length < MAX_NAME_LENGTH
-    isValid = firstName.value.length > 0
-    isValid = !containsNumbers(firstName.value)
-    
-    isValid = lastName.value.length < MAX_NAME_LENGTH
-    isValid = lastName.value.length > 0
-    isValid = !containsNumbers(lastName.value)
-    
-    isValid = containsNumbers(password.value)
-    IsValid = password.value === confirmpw.value
+const validateName = (name) => {
+    return name.length < MAX_NAME_LENGTH && name.length > 0 && !containsNumbers(name);
+};
+
+const validatePassword = (password, confirmation) => {
+    return containsNumbers(password) && password === confirmation;
+};
+
+const validate = () => {
+    let isValid = true;
+
+    isFirstNameValid = validateName(firstName.value);
+    isLastNameValid = validateName(lastName.value);
+    isPasswordValid = validatePassword(password.value, confirmpw.value);
 
     debug();
-    console.log("----------")
 
-    console.log(`is valid? : ${isValid}`)
+    console.log("----------");
 
-    return isValid
-}
+    isValid = isFirstNameValid && isLastNameValid && isPasswordValid;
+    console.log(`is valid? : ${isValid}`);
+
+    return isValid;
+};
 
 function debug() {
-    firstName.value.length < MAX_NAME_LENGTH ?    console.log("") : console.log(`ERROR: el nombre no puede superar el tamaño máximo de ${MAX_NAME_LENGTH}`)
-    firstName.value.length > 0 ?                  console.log("") : console.log("ERROR: el nombre no debe estar vacío")
-    !(containsNumbers(firstName.value)) ?         console.log("") : console.log("ERROR: el nombre no debe contener números")
-    lastName.value.length < MAX_NAME_LENGTH ?     console.log("") : console.log(`ERROR: el apellido no puede superar el tamaño máximo de ${MAX_NAME_LENGTH}`)
-    lastName.value.length > 0 ?                   console.log("") : console.log(`ERROR: el apellido no debe estar vacío`)
-    !(containsNumbers(lastName.value)) ?          console.log("") : console.log(`ERROR: el apellido no debe contener números`)
-    containsNumbers(password.value) ?             console.log("") : console.log("ERROR: la contraseña debe contener números")
-    password.value === confirmpw.value ?          console.log("")  : console.log("ERROR: las contraseñas deben coincidir")
+    firstName.value.length < MAX_NAME_LENGTH ? console.log("") : console.log(`ERROR: el nombre no puede superar el tamaño máximo de ${MAX_NAME_LENGTH}`);
+    firstName.value.length > 0 ? console.log("") : console.log("ERROR: el nombre no debe estar vacío");
+    !containsNumbers(firstName.value) ? console.log("") : console.log("ERROR: el nombre no debe contener números");
+    lastName.value.length < MAX_NAME_LENGTH   ? console.log("")
+    : console.log(`ERROR: el apellido no puede superar el tamaño máximo de ${MAX_NAME_LENGTH}`);
+    lastName.value.length > 0 ? console.log("") : console.log(`ERROR: el apellido no debe estar vacío`);
+    !containsNumbers(lastName.value) ? console.log("") : console.log(`ERROR: el apellido no debe contener números`);
+    containsNumbers(password.value) ? console.log("") : console.log("ERROR: la contraseña debe contener números");
+    password.value === confirmpw.value ? console.log("") : console.log("ERROR: las contraseñas deben coincidir");
+
+}
+
+const saveData = () => {
+    localStorage.setItem('first-name', firstName.value)
+    localStorage.setItem('last-name', lastName.value)
+    localStorage.setItem('username', username.value)
+    localStorage.setItem('password', password.value)
+    localStorage.setItem('birthday', birthday.value)
+    localStorage.setItem('newsletter', checkbox.value)
+    console.log(localStorage.getItem('first-name'))
 }
 
 button.addEventListener("click", () => {
-    if (validate())
-    {
-        window.alert(`Gracias por ceder tus datos ${firstName.value}`)
+    if (validate()) {
+        saveData()
+    } else {
+        window.alert("ERROR: Verifique los campos");
     }
-    else
-    {
-        window.alert("ERROR: Verifique los campos")
+});
+
+window.addEventListener("input", validate);
+
+allNames.forEach((name) => {
+    name.addEventListener("input", () => {
+    if (validateName(name.value)){ 
+        name.classList.remove("invalid") 
+    } else {
+        name.classList.add("invalid");  
     }
+    });
+});
+
+firstName.addEventListener("change", () => {
+        isFirstNameValid ? warnings[0].classList.add("hidden") : warnings[0].classList.remove("hidden")
 })
 
-window.addEventListener("change", validate);
+lastName.addEventListener("change", () => {
+        isLastNameValid ? warnings[1].classList.add("hidden") : warnings[1].classList.remove("hidden")
+})
 
-function containsNumbers(str)
-{
+confirmpw.addEventListener("change", () => {
+        isPasswordValid ? warnings[2].classList.add("hidden") : warnings[2].classList.remove("hidden")
+})
+
+
+
+
+confirmpw.addEventListener("change", () => {
+    if (validatePassword(password.value, confirmpw.value)){
+        password.classList.remove("invalid") 
+        confirmpw.classList.remove("invalid")
+    }
+    else {
+        password.classList.add("invalid")
+        confirmpw.classList.add("invalid")
+    } 
+})
+
+
+
+function containsNumbers(str) {
     const regex = /[0-9]/g;
     return regex.test(str);
 }
+
+const dataButton = document.getElementById("showdata")
+dataButton.addEventListener("click", () => {
+    console.log(window.localStorage.getItem('first-name'))
+    console.log(window.localStorage.getItem('last-name'))
+    console.log(window.localStorage.getItem('username'))
+    console.log(window.localStorage.getItem('password'))
+    console.log(window.localStorage.getItem('birthday'))
+    console.log(window.localStorage.getItem('newsletter'))
+})
